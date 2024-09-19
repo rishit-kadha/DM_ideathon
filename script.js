56b5258a4283eb7e3fdc419098ef7fcf02f48eb1
Rishit Kadha <115226865+rishit-kadha@users.noreply.github.com>
2024-09-19 06:54:23 +0530

    made structure for before

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const noInternet=document.getElementsByClassName("noInternet");
-const PreDisaster=document.getElementsByClassName("PreDisaster");
-const DuringDisaster=document.getElementsByClassName("DuringDisaster");
-const PostDisaster=document.getElementsByClassName("PostDisaster");
+const noInternet=document.getElementsByClassName("noInternet")[0];
+const PreDisaster=document.getElementsByClassName("PreDisaster")[0];
+const DuringDisaster=document.getElementsByClassName("DuringDisaster")[0];
+const PostDisaster=document.getElementsByClassName("PostDisaster")[0];
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
@@ -9,3 +9,35 @@ function closeNav() {
 function openNav() {
     document.getElementById("mySidenav").style.width = "50vw";
   }
+function openSectionNoInternet() {
+  if(noInternet.classList.contains("hidden")){
+    noInternet.classList.remove("hidden");
+    PreDisaster.classList.add("hidden");
+    DuringDisaster.classList.add("hidden");
+    PostDisaster.classList.add("hidden");
+  }
+}
+function openSectionPreDisaster() {
+  if(PreDisaster.classList.contains("hidden")){
+    noInternet.classList.add("hidden");
+    PreDisaster.classList.remove("hidden");
+    DuringDisaster.classList.add("hidden");
+    PostDisaster.classList.add("hidden");
+  }
+}
+function openSectionDuringDisaster() {
+  if(DuringDisaster.classList.contains("hidden")){
+    noInternet.classList.add("hidden");
+    PreDisaster.classList.add("hidden");
+    DuringDisaster.classList.remove("hidden");
+    PostDisaster.classList.add("hidden");
+  }
+}
+function openSectionPostDisaster() {
+  if(PostDisaster.classList.contains("hidden")){
+    noInternet.classList.add("hidden");
+    PreDisaster.classList.add("hidden");
+    DuringDisaster.classList.add("hidden");
+    PostDisaster.classList.remove("hidden");
+  }
+}
